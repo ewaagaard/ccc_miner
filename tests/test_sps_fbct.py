598e62e132687a91d4ee5f3e9d70131c0163d3b6
elwaagaa <elias.walter.waagaard@cern.ch>
2023-09-27 11:25:16 +0200

    Added SPS superclass

--- tests/test_sps_fbct.py
+++ tests/test_sps_fbct.py
@@ -1,11 +1,11 @@
 """
 Test script to properly read and fBCT file from SPS
 """
-from ccc_miner import SPS
-
-# Instantiate class 
-sps = SPS()
+from ccc_miner import FBCT
 
 # Load data into fBCT
 parquet_file = 'test_data/2023.09.22.16.07.26.612797.parquet'
-sps.FBCT(parquet_file)
+
+# Instantiate class 
+fbct = FBCT(parquet_file)
+fbct.plot()
